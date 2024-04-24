11cacf6c7e9208469d95275ed9ee0c517225cbc3
Amaechina <amaechinaikechukwu6@gmail.com>
2024-04-24 10:02:31 +0100

    key missing error fixed

--- src/app/(business)/error.tsx
+++ src/app/(business)/error.tsx
@@ -13,6 +13,7 @@ export default function Error({
   return (
     <div className="w-full h-screen bg-orange-500 flex flex-col  items-center justify-center">
       <h2>Something went wrong!</h2>
+      <p>{error && JSON.stringify(error)}</p>
     </div>
   );
 }
